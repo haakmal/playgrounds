4a0350846b57a532f2e167eff4f6eea25b603a82
haakmal <36928871+haakmal@users.noreply.github.com>
2025-07-16 15:20:38 +1000

    Inject FA directly into JS return button

--- assets/inject-home-link.js
+++ assets/inject-home-link.js
@@ -1,5 +1,13 @@
 // inject-home-link.js
 (function () {
+	// Inject Font Awesome stylesheet
+	const fa = document.createElement("link");
+	fa.rel = "stylesheet";
+	fa.href = "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.2/css/all.min.css";
+	fa.crossOrigin = "anonymous";
+	document.head.appendChild(fa);
+	
+	// Create the back link
 	const link = document.createElement("a");
 	link.href = "/playgrounds/";
 	link.innerHTML = '← Back to <i class="fab fa-github"><i> Playgrounds';
@@ -12,6 +20,7 @@
 	link.style.borderRadius = "6px";
 	link.style.textDecoration =  "none";
 	link.style.zIndex = "1000";
+	link.style.fontSize = "14px";
 	link.style.opacity = "0.7";
 	link.onmouseover = () => (link.style.opacity = "1");
 	link.onmouseout = () => (link.style.opacity = "0.7");
